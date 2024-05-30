ab201f031aba16d35b87513ed7a608ef8b7f9d45
owx0130 <weixiang0130@gmail.com>
2024-05-31 01:31:53 +0800

    reformat tidal data databases, fix algo error in tide_getter

--- tide_getter.py
+++ tide_getter.py
@@ -11,25 +11,25 @@ vessel_arrival = file.to_numpy()
 
 # retrive 2022 tide data from csv file
 # change usecols parameter if required. retrieved data should be in DD:"HH:MM":{"H", "L"} format
-file = pd.read_csv("tidal_data/2022_tidal_data.csv", header=None, usecols=[1,2,4], na_filter=False)
+file = pd.read_csv("tidal_data/2022_tidal_data.csv", header=None, na_filter=False)
 tide_2022 = file.to_numpy()
-print(tide_2022)
+# print(tide_2022)
 
 # retrive 2023 tide data from csv file
 # change usecols parameter if required. retrieved data should be in DD:"HH:MM":{"H", "L"} format
-file = pd.read_csv("tidal_data/2023_tidal_data.csv", header=None, usecols=[1,2,4], na_filter=False)
+file = pd.read_csv("tidal_data/2023_tidal_data.csv", header=None, na_filter=False)
 tide_2023 = file.to_numpy()
-print(tide_2023)
+# print(tide_2023)
 
 # retrive 2024 tide data from csv file
 # change usecols parameter if required. retrieved data should be in DD:"HH:MM":{"H", "L"} format
-file = pd.read_csv("tidal_data/2024_tidal_data.csv", header=None, usecols=[1,2,4], na_filter=False)
+file = pd.read_csv("tidal_data/2024_tidal_data.csv", header=None, na_filter=False)
 tide_2024 = file.to_numpy()
-print(tide_2024)
+# print(tide_2024)
 
 tide_data = []
 # days - 0, months - 1, years - 2, time - 3
-for i in range(len(vessel_arrival)):
+for i in range(10):
   day = str(vessel_arrival[i, 0])
   month = months_ref[vessel_arrival[i, 1]]
   year = vessel_arrival[i, 2]
@@ -45,15 +45,18 @@ for i in range(len(vessel_arrival)):
   elif year == 2024:
     month_idx = np.argwhere(tide_2024[:, 0] == month)[0][0]
     truncated = tide_2024[month_idx:]
-
+  
   day_idx = np.argwhere(truncated == str(day))[0][0]
-  while(day_idx < len(truncated) and time > truncated[day_idx, 1]):
+  prev_time = ""
+  while(day_idx < len(truncated) and time > truncated[day_idx, 2] and prev_time < truncated[day_idx, 2]):
+    print(day, truncated[day_idx, 2])
+    prev_time = truncated[day_idx, 2]
     day_idx += 1
 
-  if (day_idx == len(truncated) or truncated[day_idx, 1] == "Time"):
-    tide = truncated[day_idx - 1, 2]
+  if (day_idx == len(truncated)):
+    tide = truncated[day_idx - 1, 4]
   else:
-    tide = "H" if truncated[day_idx, 2] == "L" else "L"
+    tide = "H" if truncated[day_idx, 4] == "L" else "L"
   
   tide_data.append(tide)
 
