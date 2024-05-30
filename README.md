e5e10e14f40c7761b75ac40f81870f7f0e8fc2ef
owx0130 <weixiang0130@gmail.com>
2024-05-30 23:44:10 +0800

    fix index out of range, used cleaner tidal dataset to work with

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # da-test
+
+## Get tidal level based on vessel arrival time
+
+Tidal data obtained from [here](http://www.weather.gov.sg/weather-astronomical-and-tidal-information-monthly-data/).
+
+2024 tidal data only taken up till Mar 24.
+
+To use, drag and drop the `vessel_arrival` dataset into the root directory. Information on the format of the csv file is written in `tide_getter.py`. A sample dataset for reference is given in this repository.
+
+`out.csv` contains the tide level at the vessel's time of arrival.
--- tide_getter.py
+++ tide_getter.py
@@ -8,23 +8,22 @@ months_ref = {1: "Jan", 2: "Feb", 3: "Mar", 4: "Apr", 5: "May", 6: "Jun", 7: "Ju
 # change usecols parameter if required. retrieved data should be in DD:MM:YYYY:"HH:MM" format
 file = pd.read_csv("vessel_arrival.csv", header=None, usecols=[2,3,4,5])
 vessel_arrival = file.to_numpy()
-print(vessel_arrival)
 
 # retrive 2022 tide data from csv file
 # change usecols parameter if required. retrieved data should be in DD:"HH:MM":{"H", "L"} format
-file = pd.read_csv("2022_tide.csv", header=None, usecols=[1,7,9], na_filter=False)
+file = pd.read_csv("tidal_data/2022_tidal_data.csv", header=None, usecols=[1,2,4], na_filter=False)
 tide_2022 = file.to_numpy()
 print(tide_2022)
 
 # retrive 2023 tide data from csv file
 # change usecols parameter if required. retrieved data should be in DD:"HH:MM":{"H", "L"} format
-file = pd.read_csv("2023_tide.csv", header=None, usecols=[1,7,9], na_filter=False)
+file = pd.read_csv("tidal_data/2023_tidal_data.csv", header=None, usecols=[1,2,4], na_filter=False)
 tide_2023 = file.to_numpy()
 print(tide_2023)
 
 # retrive 2024 tide data from csv file
 # change usecols parameter if required. retrieved data should be in DD:"HH:MM":{"H", "L"} format
-file = pd.read_csv("2024_tide.csv", header=None, usecols=[1,7,9], na_filter=False)
+file = pd.read_csv("tidal_data/2024_tidal_data.csv", header=None, usecols=[1,2,4], na_filter=False)
 tide_2024 = file.to_numpy()
 print(tide_2024)
 
@@ -37,41 +36,26 @@ for i in range(len(vessel_arrival)):
   time = str(vessel_arrival[i, 3])
   if year == 2022:
     month_idx = np.argwhere(tide_2022[:, 0] == month)[0][0]
-    day_idx = np.argwhere(tide_2022[:, 0][month_idx:] == str(day))[0][0]
-    while(tide_2022[day_idx + 1, 1] != "" and tide_2022[day_idx + 1, 1] > tide_2022[day_idx, 1]):
-      if (time < tide_2022[day_idx, 1]):
-        tide = "H" if tide_2022[day_idx, 2] == "L" else "L"
-        break
-      day_idx += 1
-
-    if tide_2022[day_idx + 1, 1] == "" or tide_2022[day_idx + 1, 1] < tide_2022[day_idx, 1]:
-      tide = tide_2022[day_idx, 2]
+    truncated = tide_2022[month_idx:]
     
   elif year == 2023:
     month_idx = np.argwhere(tide_2023[:, 0] == month)[0][0]
-    day_idx = np.argwhere(tide_2023[:, 0][month_idx:] == str(day))[0][0]
-    while(tide_2023[day_idx + 1, 1] != "" and tide_2023[day_idx + 1, 1] > tide_2023[day_idx, 1]):
-      if (time < tide_2023[day_idx, 1]):
-        tide = "H" if tide_2023[day_idx, 2] == "L" else "L"
-        break
-      day_idx += 1
-
-    if tide_2023[day_idx + 1, 1] == "" or tide_2023[day_idx + 1, 1] < tide_2023[day_idx, 1]:
-      tide = tide_2023[day_idx, 2]
+    truncated = tide_2023[month_idx:]
 
   elif year == 2024:
     month_idx = np.argwhere(tide_2024[:, 0] == month)[0][0]
-    day_idx = np.argwhere(tide_2024[:, 0][month_idx:] == str(day))[0][0]
-    while(tide_2024[day_idx + 1, 1] != "" and tide_2024[day_idx + 1, 1] > tide_2024[day_idx, 1]):
-      if (time < tide_2024[day_idx, 1]):
-        tide = "H" if tide_2024[day_idx, 2] == "L" else "L"
-        break
-      day_idx += 1
+    truncated = tide_2024[month_idx:]
 
-    if tide_2024[day_idx + 1, 1] == "" or tide_2024[day_idx + 1, 1] < tide_2024[day_idx, 1]:
-      tide = tide_2024[day_idx, 2]
+  day_idx = np.argwhere(truncated == str(day))[0][0]
+  while(day_idx < len(truncated) and time > truncated[day_idx, 1]):
+    day_idx += 1
+
+  if (day_idx == len(truncated) or truncated[day_idx, 1] == "Time"):
+    tide = truncated[day_idx - 1, 2]
+  else:
+    tide = "H" if truncated[day_idx, 2] == "L" else "L"
   
   tide_data.append(tide)
 
 df = pd.DataFrame(data=tide_data)
-df.to_csv("tide_data.csv", header=False, index=False)
+df.to_csv("out.csv", header=False, index=False)
