f6327f5185f61e8382aa6e2d48aebaf896506409
owx0130 <weixiang0130@gmail.com>
2024-06-19 17:09:02 +0800

    update tide getting function

--- tide_getter.py
+++ tide_getter.py
@@ -6,30 +6,27 @@ months_ref = {1: "Jan", 2: "Feb", 3: "Mar", 4: "Apr", 5: "May", 6: "Jun", 7: "Ju
 
 # retrive vessel arrival data from csv file
 # change usecols parameter if required. retrieved data should be in DD:MM:YYYY:"HH:MM" format
-file = pd.read_csv("vessel_arrival.csv", header=None, usecols=[2,3,4,5])
+file = pd.read_csv("vessel_arrival_new.csv", usecols=[3,4,5,6])
 vessel_arrival = file.to_numpy()
 
 # retrive 2022 tide data from csv file
 # change usecols parameter if required. retrieved data should be in DD:"HH:MM":{"H", "L"} format
 file = pd.read_csv("tidal_data/2022_tidal_data.csv", header=None, na_filter=False)
 tide_2022 = file.to_numpy()
-# print(tide_2022)
 
 # retrive 2023 tide data from csv file
 # change usecols parameter if required. retrieved data should be in DD:"HH:MM":{"H", "L"} format
 file = pd.read_csv("tidal_data/2023_tidal_data.csv", header=None, na_filter=False)
 tide_2023 = file.to_numpy()
-# print(tide_2023)
 
 # retrive 2024 tide data from csv file
 # change usecols parameter if required. retrieved data should be in DD:"HH:MM":{"H", "L"} format
 file = pd.read_csv("tidal_data/2024_tidal_data.csv", header=None, na_filter=False)
 tide_2024 = file.to_numpy()
-# print(tide_2024)
 
 tide_data = []
 # days - 0, months - 1, years - 2, time - 3
-for i in range(10):
+for i in range(len(vessel_arrival)):
   day = str(vessel_arrival[i, 0])
   month = months_ref[vessel_arrival[i, 1]]
   year = vessel_arrival[i, 2]
@@ -49,7 +46,6 @@ for i in range(10):
   day_idx = np.argwhere(truncated == str(day))[0][0]
   prev_time = ""
   while(day_idx < len(truncated) and time > truncated[day_idx, 2] and prev_time < truncated[day_idx, 2]):
-    print(day, truncated[day_idx, 2])
     prev_time = truncated[day_idx, 2]
     day_idx += 1
 
@@ -61,4 +57,4 @@ for i in range(10):
   tide_data.append(tide)
 
 df = pd.DataFrame(data=tide_data)
-df.to_csv("out.csv", header=False, index=False)
+df.to_csv("tides.csv", header=False, index=False)
